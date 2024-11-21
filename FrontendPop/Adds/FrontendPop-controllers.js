a934dae35ca14a0419e221f9bd38b7033f1cb081
Javier Rodriguez <franciscoromera08162gmail.com>
2024-11-21 22:59:28 +0100

    add frontendPop models 1 version

--- FrontendPop/Adds/FrontendPop-controllers.js
+++ FrontendPop/Adds/FrontendPop-controllers.js
@@ -1,3 +1,4 @@
 // Importar las funciones desde las vistas y modelos
-
-  
+export async function addController () {
+    
+}
--- FrontendPop/Adds/FrontendPop-models.js
+++ FrontendPop/Adds/FrontendPop-models.js
@@ -1,3 +1,14 @@
 // FrontendPop-models.js
-
+export async function getAdds () {
+    try {
+        const response = await fetch("http://localhost8000/api/adds");
+        const adds = await response.json();
+        if(!response.ok) {
+            throw new Error('Not Found')
+        };
+        return adds;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
 
